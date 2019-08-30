20fd8ae7a9dae49ea2b70b79e39368b28d8dccc6
Lari Taskula <lari.taskula@hypernova.fi>
2019-08-30 21:28:02 +0000

    Document export_dump.sh

--- export_dump.sh
+++ export_dump.sh
@@ -5,5 +5,6 @@ PASSWORD="hypernova"
 DATABASE="hypernova"
 
 ARGS="--user=$USERNAME --password=$PASSWORD --opt --skip-dump-date"
+# https://stackoverflow.com/questions/15750535/using-mysqldump-to-format-one-insert-per-line
 /usr/bin/mysqldump $ARGS $DATABASE | sed 's$VALUES ($VALUES\n($g' | sed 's$),($),\n($g' > hypernova.sql
 echo "DELETE FROM wp_options WHERE option_name='wpml_language_switcher_template_objects';\n" >> hypernova.sql
