3a2515d8abfd17bdaa701ad9ce5c9aef5dce0d89
Lari Taskula <lari.taskula@hypernova.fi>
2019-09-16 22:46:59 +0000

    Default URL change

--- import_database.sh
+++ import_database.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v wp)" ]; then
   exit 1
 fi
 
-read -e -p "Enter site URL: " -i "https://www.hypernova.fi" SITE_URL
+read -e -p "Enter site URL: " -i "http://homepage" SITE_URL
 
 read -e -p "Enter database name: " -i "hypernova" DB_NAME
 read -e -p "Enter database user: " -i "hypernova" DB_USER
